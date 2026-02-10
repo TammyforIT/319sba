314e0cbdabe12b55ce48910062b892a0211396db
TammyforIT <tamtam113.tg@gmail.com>
2026-02-10 13:36:37 -0500

    Update vets.js

--- routes/vets.js
+++ routes/vets.js
@@ -3,21 +3,40 @@ import Vet from "../models/Vet.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.json(await Vet.find());
+router.get("/", async (req, res, next) => {
+  try {
+    const vets = await Vet.find();
+    res.json(vets);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/", async (req, res) => {
-  res.json(await Vet.create(req.body));
+router.post("/", async (req, res, next) => {
+  try {
+    const newVet = await Vet.create(req.body);
+    res.json(newVet);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.patch("/:id", async (req, res) => {
-  res.json(await Vet.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const updated = await Vet.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete("/:id", async (req, res) => {
-  await Vet.findByIdAndDelete(req.params.id);
-  res.json({ message: "Vet deleted" });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Vet.findByIdAndDelete(req.params.id);
+    res.json({ message: "Vet deleted" });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
